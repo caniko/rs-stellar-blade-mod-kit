e3935feca4a20832f6c8b81e71ee2ac2715863f7
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-17 23:40:34 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
     crane.follows = "rs-harbor/crane";
-    flake-utils.follows = "rs-harbor/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
