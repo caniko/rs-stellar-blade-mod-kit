e3e15cb65b8b9b070b337f07deaecf8ad222dc35
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-18 23:38:40 +0200

    chore: point moved nix inputs at github

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   inputs = {
     rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=77d0a937c760e6ced8b7ec8fc5a214f550abe35e";
 
-    rs-harbor-macos-sdk-pin.url = "git+ssh://git@codeberg.org/caniko/rs-harbor-macos-sdk-pin.git";
+    rs-harbor-macos-sdk-pin.url = "git+https://github.com/caniko/rs-harbor-macos-sdk-pin.git";
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
